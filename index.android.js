ae57b378a0bb5c97629b0d1b29164a4e0730a4d3
zhongjie-chen <czjchn@163.com>
2016-05-19 16:24:45 +0800

    0.26 nice

--- index.android.js
+++ index.android.js
@@ -10,42 +10,15 @@ import {
   Text,
   View
 } from 'react-native';
-
+import Main from './app/Main';
 class awsomeShowcase extends Component {
   render() {
     return (
-      <View style={styles.container}>
-        <Text style={styles.welcome}>
-          Welcome to React Native!
-        </Text>
-        <Text style={styles.instructions}>
-          To get started, edit index.android.js
-        </Text>
-        <Text style={styles.instructions}>
-          Shake or press menu button for dev menu
-        </Text>
-      </View>
+      <Main></Main>
     );
   }
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
-  },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
-  },
-});
+
 
 AppRegistry.registerComponent('awsomeShowcase', () => awsomeShowcase);
